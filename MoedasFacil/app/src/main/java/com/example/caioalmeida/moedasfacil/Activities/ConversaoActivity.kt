a54b6c4f0f5cc5180b95921642772f910cfbff0a
Caio Azevedo <caioba19@gmail.com>
2018-10-22 11:49:27 -0300

    Projeto de Conversão e Cotação de Moedas com funcionalidade de Cotação

---
+++ MoedasFacil/app/src/main/java/com/example/caioalmeida/moedasfacil/Activities/ConversaoActivity.kt
@@ -0,0 +1,13 @@
+package com.example.caioalmeida.moedasfacil.Activities
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.caioalmeida.moedasfacil.R
+
+class ConversaoActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_conversao)
+    }
+}
